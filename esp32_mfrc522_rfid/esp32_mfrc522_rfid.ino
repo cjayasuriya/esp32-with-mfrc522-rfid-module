cc75342c18b62fc3bb031dab7d60f81c9f77dc3d
Chamal Jayasuriya <chamaljaya@gmail.com>
2023-01-09 10:28:52 +0530

    Adding RFID and LCD controls

--- esp32_mfrc522_rfid/esp32_mfrc522_rfid.ino
+++ esp32_mfrc522_rfid/esp32_mfrc522_rfid.ino
@@ -1,9 +1,57 @@
+#include <iostream>
+#include <string>
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define buzzerPin 15
+
+String tagID = "";
+
+LiquidCrystal_I2C lcd(0x27, 16, 4);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  lcd.begin();
+  lcd.backlight();
+
+  lcd.setCursor(2, 1);
+  lcd.print("Initializing");
+
+  lcd.setCursor(-3, 2);
+  lcd.print("Please wait...");
+
+  delay(1000);
+
+  initIOs();
+  initRFID();
+
+  lcd.clear();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  if (isRFIDDetected()) {
+    Serial.println(tagID);
+    lcd.clear();
+    displayRFID(tagID);
+  }
+
+}
+
+void displayRFID(String uid) {
+  lcd.setCursor(-4, 2);
+  lcd.print("RFID: " + uid);
+}
+
+void initIOs() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void buzzerBeep(int delayMS) {
+  digitalWrite(buzzerPin, HIGH);
+  delay(delayMS);
+  digitalWrite(buzzerPin, LOW);
 }
---
+++ esp32_mfrc522_rfid/rfidThings.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 4
+#define SS_PIN 5
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void initRFID(){
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+//Read tag if available
+boolean isRFIDDetected()
+{
+
+  tagID = "";
+
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+
+      for (int i = 0; i < rfid.uid.size; i++) {
+        tagID.concat(String(rfid.uid.uidByte[i], HEX));
+      }
+
+      tagID.toUpperCase();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+
+  }
+
+  if (tagID.length() > 6) {
+    buzzerBeep(100);
+    return true;
+  } else {
+    return false;
+  }
+
+}
